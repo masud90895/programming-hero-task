dd514c715cd5616781b0b5048b199c58b4b37a5f
masud90895 <masudhossainmbs129@gmail.com>
2023-01-29 13:18:51 +0600

    login and registation form added

--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import logo from "../../assists/Power Hack.jpeg"
+import logo from "../../assists/Power Hack(white).png"
 
 const Header = () => {
     return (
-        <div className='flex justify-between bg-[#8ecae6] md:px-10 px-4 py-3'>
+        <div className='flex justify-between bg-[#8ecae6] md:px-10 px-4 py-3 text-lg'>
             {/* <h1 className='hover:text-[#023047]'>img</h1> */}
-            <img src={logo} alt="" className='w-[80px]' />
+            <div>
+                <Link to='/'> <img src={logo} alt="" className='w-[150px] h-full' /></Link>
+            </div>
             <div className='flex  gap-3'>
                 <h1>Paid Total : <span>0</span></h1>
                 <Link to='../login' className='bg-'>Log out</Link>
--- src/Pages/Table/Table.js
+++ src/Pages/Table/Table.js
@@ -3,13 +3,13 @@ import React from 'react';
 const Table = () => {
     return (
         <div className='mt-10 text-xl'>
-            <div className='md:flex justify-between bg-[#8ecae6] py-3 md:px-12 '>
-                <div className='flex gap-3 items-center'>
-                    <h1>Billing</h1>
-                    <input type="text" placeholder='Search...' className='border rounded-lg p-1'/>
+            <div className='md:flex justify-between bg-[#8ecae6] py-3 md:px-12 rounded px-4'>
+                <div className='flex md:gap-3 gap-2 items-center'>
+                    <h1 className='text-2xl '>Billing</h1>
+                    <input type="text" placeholder='Search...' className='border rounded-lg p-1 '/>
                 </div>
-                <div>
-                    <button>Add New Bill</button>
+                <div className='mt-5 md:mt-0'>
+                    <button className='text-black bg-white py-1 px-4 rounded-lg border border-white hover:bg-[#8ecae6]  '>Add New Bill</button>
                 </div>
             </div>
         </div>
